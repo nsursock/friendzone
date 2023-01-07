6d435044e715796de34829a52c29ee2a97dc954a
Nicolas Sursock <nicolask@Nicolass-MacBook-Pro.local>
2023-01-07 12:24:47 +0100

    third commit

--- api/auth.js
+++ api/auth.js
@@ -9,13 +9,28 @@ export default async function handler(request, response) {
     )
 
     const { email, password } = request.body
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
 
-    response.status(200).send({ success: true, user: data })
-    
+    if (request.query.mode === 'signup') {
+      const { user, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+      })
+      if (error)
+        response.status(400).send({ success: false, error: error.message })
+      else
+        response.status(200).send({ success: true, user: user })
+
+    } else if (request.query.mode === 'login') {
+      const { user, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      })
+      if (error)
+        response.status(400).send({ success: false, error: error.message })
+      else
+        response.status(200).send({ success: true, user: user })
+    }
+
   } catch (error) {
     console.log(error)
     response
