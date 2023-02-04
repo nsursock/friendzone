36f2a98ee122b9423141b23318c1d72a04a5d88e
Nicolas Sursock <nicolask@Samerlipopette-MacBook-Pro.local>
2023-02-04 19:59:12 +0100

    limit to 100 posts while waiting for infinite scroll

--- message.js
+++ message.js
@@ -3,7 +3,6 @@ const { createClient } = require('@supabase/supabase-js')
 require('dotenv').config()
 const { faker } = require('@faker-js/faker');
 
-console.log('starting up');
 
 (async () => {
   try {
@@ -13,22 +12,45 @@ console.log('starting up');
       process.env.SUPABASE_KEY
     )
 
-    const table = 'messages.dev'
+    var table = 'relationships.dev'
+    const email = 'nicolas.sursock@gmail.com'
+    const statuses = ['Accepted']
 
-    var emails = (await supabase.from('users.dev').select('email')).data.map((item) => item.email).sort(() => 0.5 - Math.random())
-    var ids = (await supabase.from('messages.dev').select('id').not( "related_id", "is", null )).data.map((item) => item.id).sort(() => 0.5 - Math.random())
+    var data1 = (await supabase.from(table)
+      .select(`user1 (id, first_name, last_name, avatar_url, cover_url, title, city, email, phone_number, country, birthday, description)`)
+      .eq('user2', email).in('status', statuses)).data
+    data1 = JSON.parse(JSON.stringify(data1).split('"user1":').join('"friend":'))
+    console.log(data1);
 
-    const num = 1000
-    // const ids = [4673,4674,4675,4676,4677]
+    var data2 = (await supabase.from(table)
+      .select(`user2 (id, first_name, last_name, avatar_url, cover_url, title, city, email, phone_number, country, birthday, description)`)
+      .eq('user1', email).in('status', statuses)).data
+    data2 = JSON.parse(JSON.stringify(data2).split('"user2":').join('"friend":'))
+    console.log(data2);
+
+    var data = data1.concat(data2)
+
+    var table = 'messages.dev'
+
+    var emails = data.map(d => d.friend.email)
+    var ids = (await supabase.from('messages.dev').select('id').in('author', emails)).data.map((item) => item.id).sort(() => 0.5 - Math.random())
+    // .not("related_id", "is", null))
+      
+
+    // var emails = (await supabase.from('users.dev').select('email')).data.map((item) => item.email).sort(() => 0.5 - Math.random())
+    // var ids = (await supabase.from('messages.dev').select('id').not("related_id", "is", null)).data.map((item) => item.id).sort(() => 0.5 - Math.random())
+
+    const num = 20
+    var ids = [7979, 8024]
     for (let index = 0; index < num; index++) {
 
       var { data, error } = await supabase.from(table).insert({
-        related_id: ids[index%ids.length],
-        created_at: faker.date.recent(10),
+        related_id: ids[index % ids.length],
+        created_at: faker.date.recent(20),
         author: emails[index % emails.length],
         content: faker.lorem.text(),
-        num_like: Math.round(Math.random() * 100),
-        num_impr: Math.round(Math.random() * 1000)
+        num_like: Math.round(Math.random() * 10),
+        num_impr: Math.round(Math.random() * 100)
       }).select('*')
 
       console.log({
